82458d23e1cba416d1e606a203cb36cd431e2d83
Chris Ahl <cahl@redhat.com>
2022-07-05 11:41:05 -0400

    initial steps to get E2E tests from prow going
    Signed-off-by: Chris Ahl <cahl@redhat.com>

---
+++ test/e2e/install-compute-operator.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Copyright Red Hat
+
+set -e
+
+echo "--- Install the compute operator ..."
+
+#idp_dir=$(mktemp -d -t idp-XXXXX)
+#cd "$idp_dir" || exit 1
+#export HOME="$idp_dir"
+
+## Set up repo URLs.
+#echo "--- Cloning branch idp-mgmt-operator ${PULL_BASE_REF}"
+#compue_operator_url="https://${COMPUTE_OPERATOR_REPO}.git"
+#compute_operator_dir="${idp_dir}/idp-mgmt-operator"
+#git clone -b "${PULL_BASE_REF}" "$compute_operator_url" "$compute_operator_dir" || {
+#    echo "ERROR Could not clone branch ${PULL_BAWE_REF} from compute-operator repo $compute_operator_url"
+#    exit 1
+#}
+
+cd ${COMPUTE_OPERATOR_DIR}
+
+export IMG="quay.io/stolostron/compute-operator:2.6.0-PR${PULL_NUMBER}-${PULL_PULL_SHA}"
+echo "--- Quay image is ${IMG}"
+
+echo "--- Check namespace - before"
+oc get namespaces
+
+
+
+echo "--- Create compute-config namespace"
+oc new-project compute-config
+oc project compute-config
+
+
+echo "--- Start deploy"
+#export CATALOG_DEPLOY_NAMESPACE=idp-mgmt-config
+make deploy
+echo "--- Sleep a bit for installer pod to start..."
+sleep 120
+
+echo "--- Check namespace - after"
+oc get namespaces
+
+echo "--- Show compute  installer deployment"
+oc get deployment -n compute-config compute-installer-controller-manager -o yaml
+
+echo "--- Check that the pod is running"
+oc wait --for=condition=ready pods --all --timeout=5m -n compute-config || {
+  echo "ERROR - No compute operator pods running!"
+  oc get pods -n compute-config
+  oc get deployment -n compute-config compute-installer-controller-manager -o yaml
+  exit 1
+}
+oc get pods -n compute-config
+oc get pods -n compute-config | grep compute-installer-controller-manager || {
+  echo "ERROR compute-installer-controller-manager pod not found!"
+  exit 1
+}
+
+
+# echo "--- Create IDPConfig"
+# cat > e2e-IDPConfig.yaml <<EOF
+# apiVersion: identityconfig.identitatem.io/v1alpha1
+# kind: IDPConfig
+# metadata:
+#   name: idp-config
+#   namespace: idp-mgmt-config
+# spec:
+# EOF
+# oc create -f e2e-IDPConfig.yaml
+#
+# sleep 20
+#
+# echo "--- Check for operator manager and webhook pods also running"
+# oc wait --for=condition=ready pods --all --timeout=5m -n idp-mgmt-config
+# oc get pods -n idp-mgmt-config
+# oc get pods -n idp-mgmt-config | grep idp-mgmt-operator-manager || {
+#   echo "ERROR idp-mgmt-operator-manager pod not found!"
+#   exit 1
+# }
+# oc get pods -n idp-mgmt-config | grep idp-mgmt-webhook-service || {
+#   echo "ERROR idp-mgmt-webhook-service pod not found!"
+#   exit 1
+# }
+
+echo "--- Done installing compute operator"
