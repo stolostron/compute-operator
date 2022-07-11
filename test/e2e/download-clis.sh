0b768e8ad9c6cfa5a43a1b17d91114016b2d5ec1
David Huynh <dhuynh@redhat.com>
2022-07-11 10:01:18 -0400

    CMCS-161: Add vcluster to e2e cli
    Signed-off-by: David Huynh <dhuynh@redhat.com>

--- test/e2e/download-clis.sh
+++ test/e2e/download-clis.sh
@@ -13,11 +13,12 @@ mv ./oc-unpacked/oc /usr/local/bin/oc
 mv ./oc-unpacked/kubectl /usr/local/bin/kubectl
 rm -rf ./oc-unpacked ./oc.tar.gz
 
-
-
 # Install jq to parse json within bash scripts
 curl -o /usr/local/bin/jq http://stedolan.github.io/jq/download/linux64/jq
 chmod +x /usr/local/bin/jq
 
+# Install vcluster to deploy virtual clusters
+curl -s -L "https://github.com/loft-sh/vcluster/releases/latest" | sed -nE 's!.*"([^"]*vcluster-linux-amd64)".*!https://github.com\1!p' | xargs -n 1 curl -L -o vcluster && chmod +x vcluster;
+mv vcluster /usr/local/bin/vcluster
 
 echo 'set up complete'
