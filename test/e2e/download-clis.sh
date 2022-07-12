f20800e98e1f1d3aebd0850bba6ba24db1c7b779
David Huynh <dhuynh@redhat.com>
2022-07-12 16:07:39 -0400

    add helm install
    Signed-off-by: David Huynh <dhuynh@redhat.com>

--- test/e2e/download-clis.sh
+++ test/e2e/download-clis.sh
@@ -4,7 +4,7 @@
 
 # Install OpenShift CLI.
 echo "Installing oc and kubectl clis..."
-curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.8.24/openshift-client-linux-4.8.24.tar.gz
+curl -kLo oc.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable-4.10/openshift-client-linux-4.10.20.tar.gz
 mkdir oc-unpacked
 tar -xzf oc.tar.gz -C oc-unpacked
 chmod 755 ./oc-unpacked/oc
@@ -12,6 +12,13 @@ chmod 755 ./oc-unpacked/kubectl
 mv ./oc-unpacked/oc /usr/local/bin/oc
 mv ./oc-unpacked/kubectl /usr/local/bin/kubectl
 rm -rf ./oc-unpacked ./oc.tar.gz
+
+echo "Installing helm..."
+curl -kLo helm.tar.gz https://get.helm.sh/helm-v3.9.0-linux-amd64.tar.gz
+mkdir helm-unpacked
+tar -xzf helm.tar.gz -C helm-unpacked
+chmod 755 ./helm-unpacked/linux-amd64/helm
+mv ./helm-unpacked/linux-amd64/helm /usr/local/bin/helm
 
 echo "Installing jq..."
 # Install jq to parse json within bash scripts
@@ -26,11 +33,9 @@ chmod +x /usr/local/bin/vcluster
 # Check vcluster installed properly and can be called
 echo "Check install with vcluster --version"
 vcluster --version
-echo "Check install with /usr/local/bin/vcluster --version"
-/usr/local/bin/vcluster --version
 
 # Install vcluster - TODO will move somewhere better 
 echo "Try vcluster create with /usr/local/bin/vcluster create my-vcluster --expose"
-/usr/local/bin/vcluster create my-vcluster --expose
+vcluster create my-vcluster-test --expose
 
 echo 'set up complete'
